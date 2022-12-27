518d98218be1bf7fc061cf7ac5bd5fa649e3fd3f
TijlDeRidder <80417938+TijlDeRidder@users.noreply.github.com>
2022-12-27 16:01:26 +0100

    Camera try 1

--- app/src/main/java/be/ehb/trends_littermeup/ui/feed/NewPostFragment.java
+++ app/src/main/java/be/ehb/trends_littermeup/ui/feed/NewPostFragment.java
@@ -1,21 +1,37 @@
 package be.ehb.trends_littermeup.ui.feed;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import be.ehb.trends_littermeup.databinding.FragmentDashboardBinding;
+import com.google.api.Context;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.List;
+import java.util.Random;
+
+import be.ehb.trends_littermeup.R;
 import be.ehb.trends_littermeup.databinding.FragmentNewpostBinding;
-import be.ehb.trends_littermeup.ui.dashboard.DashboardViewModel;
 
 public class NewPostFragment extends Fragment {
+    private static final int CAMERA_REQUEST_CODE = 1;
     private FragmentNewpostBinding binding;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         NewPostViewModel newPostViewModel =
@@ -23,10 +39,50 @@ public class NewPostFragment extends Fragment {
 
         binding = FragmentNewpostBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
+        Button addPic = root.findViewById(R.id.btn_addPic);
+        addPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takeAndSavePicture();
+            }
+        });
         return root;
     }
 
+    public void takeAndSavePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            savePicture(imageBitmap);
+        }
+    }
+
+    public void savePicture(Bitmap image) {
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + "/saved_images");
+        myDir.mkdirs();
+        Random generator = new Random();
+        int n = 10000;
+        n = generator.nextInt(n);
+        String fname = "Image-" + n + ".jpg";
+        File file = new File(myDir, fname);
+        if (file.exists())
+            file.delete();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            image.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
